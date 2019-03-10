3ebfe8eca16a7f01f5a029ab62cf28ce5839e93e
Niklas Schultz <thexsalkin@gmail.com>
2019-03-10 11:57:32 +0100

    Fixes wrong require statements
    What does this commit accomplish?
    Require works for this gem
    
    How did I accomplish/solve that?
    Changed require statements, so that they require each needed
    file individually.

--- lib/bits_in_bytes.rb
+++ lib/bits_in_bytes.rb
@@ -19,5 +19,5 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-require 'core/version'
-require 'core'
+require 'core/bit_in_byte.rb'
+require 'core/version.rb'
